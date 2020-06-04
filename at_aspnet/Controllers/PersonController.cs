65b8bd18051abe96ad9bc33ba76e1961512f5dd0
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2020-06-04 09:00:56 -0300

    Add project files.

---
+++ at_aspnet/Controllers/PersonController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using at_aspnet.Models;
+
+namespace at_aspnet.Controllers
+{
+    public class PersonController : Controller
+    {
+        public static List<Person> People { get; set; } = new List<Person>();
+        public static IEnumerable<Person> ResultList;
+
+        public IActionResult Index(string? message, string? type)
+        {
+            ViewBag.message = message;
+            ViewBag.type = type;
+            return View(People);
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        public IActionResult Update([FromQuery] Guid id)
+        {
+            var person = People.Where(x => x.Id == id).FirstOrDefault();
+            return View(person);
+        }
+
+        [HttpPost]
+        public IActionResult Save(Person model)
+        {
+            if (!ModelState.IsValid) { return View(); }
+            model.Id = Guid.NewGuid();
+            People.Add(model);
+            return RedirectToAction("Index", "Person", new { message = "Person added.", type = "alert-success" });
+        }
+
+        [HttpPost]
+        public IActionResult SaveUpdate(Guid id, Person model)
+        {
+            if (!ModelState.IsValid) { return View(); }
+
+            var updatedPerson = People.Where(x => x.Id == id).FirstOrDefault();
+            updatedPerson.FirstName = model.FirstName;
+            updatedPerson.Surname = model.Surname;
+            updatedPerson.Birthday = model.Birthday;
+
+            People.Remove(updatedPerson);
+            People.Add(updatedPerson);
+
+            return RedirectToAction("Index", "Person", new { message = "Person edited.", type = "alert-warning" });
+        }
+
+        public IActionResult Delete([FromQuery] Guid id)
+        {
+            var person = People.Where(x => x.Id == id).FirstOrDefault();
+            People.Remove(person);
+            return RedirectToAction("Index", "Person", new { message = "Person deleted.", type = "alert-danger" });
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchPeople(Person model)
+        {
+            ResultList = SearchFor(model.FirstName, model.Surname);
+            return RedirectToAction("Result", "Person", new { message = $"Found {ResultList.Count()} contact(s)", type = "alert-dark" });
+        }
+
+        public IEnumerable<Person> SearchFor(string termFirstName, string termSurname)
+        {
+            return People.Where(person =>
+                person.FirstName.Contains(termFirstName, StringComparison.InvariantCultureIgnoreCase) ||
+                person.Surname.Contains(termSurname, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public IActionResult Result(string? message, string? type)
+        {
+            ViewBag.message = message;
+            ViewBag.type = type;
+            return View(ResultList);
+        }
+    }
+}
---
+++ at_aspnet/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace at_aspnet.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The person must have a first name.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The person must have a surname.")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "The person must have a birthdate.")]
+        public DateTime Birthday { get; set; }
+    }
+}
