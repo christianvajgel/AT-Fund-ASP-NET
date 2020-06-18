4253b5db1acc41c13f8227aa5eed571cdeb2b328
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2020-06-18 18:41:38 -0300

    Database added and sorting making progress.

--- at_aspnet/Controllers/PersonController.cs
+++ at_aspnet/Controllers/PersonController.cs
@@ -1,22 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using at_aspnet.Models;
+using at_aspnet.Repository;
 
 namespace at_aspnet.Controllers
 {
     public class PersonController : Controller
     {
+
+        private PeopleRepository PeopleRepository { get; set; }
+
+        public PersonController(PeopleRepository peopleRepository) { this.PeopleRepository = peopleRepository; }
+
         public static List<Person> People { get; set; } = new List<Person>();
         public static IEnumerable<Person> ResultList;
 
         public IActionResult Index(string? message, string? type)
         {
+            // READ
+            var peopleList = this.PeopleRepository.GetAll();
+            List<Person> todayBirthdays = new List<Person>();
+            List<Person> nextBirthdays = new List<Person>();
+
+            TodayBirthdays(this.PeopleRepository.GetAll());
+            nextBirthdays = NextBirthdays(this.PeopleRepository.GetAll());
+            nextBirthdays.Sort((x, y) => x.Birthday.CompareTo(y.Birthday));
+
+            void TodayBirthdays(List<Person> peopleList) 
+            {
+                foreach (var p in peopleList) 
+                {
+                    if (p.Birthday.Day == DateTime.Now.Day && p.Birthday.Month == DateTime.Now.Month) { todayBirthdays.Add(p); }
+                }
+            }
+
+            List<Person> NextBirthdays(List<Person> peopleList)
+            {
+                List<Person> result = new List<Person>();
+                foreach (var p in peopleList)
+                {
+                    var date = p.Birthday;
+                    var year = date.Month < DateTime.Now.Month || 
+                               (date.Month == DateTime.Now.Month && date.Day <= DateTime.Now.Day) ? 
+                                    DateTime.Now.Year + 1 : DateTime.Now.Year;
+                    p.Birthday = new DateTime(year, date.Month, date.Day);
+                    result.Add(p);
+                }
+                return result;
+            }
+
+            ViewBag.todayBirthdays = todayBirthdays;
+            ViewBag.nextBirthdays = nextBirthdays;
             ViewBag.message = message;
             ViewBag.type = type;
-            return View(People);
+            return View(peopleList);
         }
 
         public IActionResult New()
@@ -24,49 +63,59 @@ namespace at_aspnet.Controllers
             return View();
         }
 
-        public IActionResult Update([FromQuery] Guid id)
-        {
-            var person = People.Where(x => x.Id == id).FirstOrDefault();
-            return View(person);
-        }
-
-        [HttpPost]
-        public IActionResult Save(Person model)
-        {
-            if (!ModelState.IsValid) { return View(); }
-            model.Id = Guid.NewGuid();
-            People.Add(model);
-            return RedirectToAction("Index", "Person", new { message = "Person added.", type = "alert-success" });
-        }
-
-        [HttpPost]
-        public IActionResult SaveUpdate(Guid id, Person model)
-        {
-            if (!ModelState.IsValid) { return View(); }
-
-            var updatedPerson = People.Where(x => x.Id == id).FirstOrDefault();
-            updatedPerson.FirstName = model.FirstName;
-            updatedPerson.Surname = model.Surname;
-            updatedPerson.Birthday = model.Birthday;
-
-            People.Remove(updatedPerson);
-            People.Add(updatedPerson);
-
-            return RedirectToAction("Index", "Person", new { message = "Person edited.", type = "alert-warning" });
-        }
-
-        public IActionResult Delete([FromQuery] Guid id)
-        {
-            var person = People.Where(x => x.Id == id).FirstOrDefault();
-            People.Remove(person);
-            return RedirectToAction("Index", "Person", new { message = "Person deleted.", type = "alert-danger" });
-        }
-
         public IActionResult Search()
         {
             return View();
         }
 
+        public IActionResult Result(string? message, string? type)
+        {
+            ViewBag.message = message;
+            ViewBag.type = type;
+            return View(ResultList);
+        }
+
+        // CREATE
+        [HttpPost]
+        public IActionResult Save(Person model)
+        {
+            if (!ModelState.IsValid) { return View(); }
+            model.Id = Guid.NewGuid();
+            PeopleRepository.Save(model);
+            return RedirectToAction("Index", "Person", new { message = "Person added.", type = "alert-success" });
+        }
+
+        // Save contact and UPDATE
+        [HttpPost]
+        public IActionResult SaveUpdate(Guid id, Person model)
+        {
+            if (!ModelState.IsValid) { return View(); }
+
+            var updatedPerson = PeopleRepository.GetById(id);
+            updatedPerson.FirstName = model.FirstName;
+            updatedPerson.Surname = model.Surname;
+            updatedPerson.Birthday = model.Birthday;
+
+            PeopleRepository.Update(updatedPerson);
+            return RedirectToAction("Index", "Person", new { message = "Person edited.", type = "alert-warning" });
+        }
+
+        // UPDATE - Search for specific person object
+        public IActionResult Update([FromQuery] Guid id)
+        {
+            var people = PeopleRepository.GetById(id);
+            return View(people);
+        }
+
+        // DELETE PERSON
+        public IActionResult Delete([FromQuery] Guid id)
+        {
+            if (!ModelState.IsValid) { return View(); }
+            PeopleRepository.Delete(id);
+            return RedirectToAction("Index", "Person", new { message = "Person deleted.", type = "alert-danger" });
+        }
+        
+        // Search for person  
         [HttpPost]
         public IActionResult SearchPeople(Person model)
         {
@@ -81,11 +130,6 @@ namespace at_aspnet.Controllers
                 person.Surname.Contains(termSurname, StringComparison.InvariantCultureIgnoreCase));
         }
 
-        public IActionResult Result(string? message, string? type)
-        {
-            ViewBag.message = message;
-            ViewBag.type = type;
-            return View(ResultList);
-        }
+        
     }
 }
---
+++ at_aspnet/Repository/PeopleRepository.cs
@@ -0,0 +1,167 @@
+﻿using at_aspnet.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Data;
+
+namespace at_aspnet.Repository
+{
+    public class PeopleRepository
+    {
+        private string ConnectionString { get; set; }
+        public PeopleRepository(IConfiguration configuration)
+        {
+            this.ConnectionString = configuration.GetConnectionString("PeopleConnection");
+        }
+
+        public void Save(Person person)
+        {
+            using (var connection = new SqlConnection(this.ConnectionString))
+            {
+                var sql = @"INSERT INTO PEOPLE(Id,FirstName,Surname,Birthday) VALUES (@P1, @P2, @P3, @P4)";
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                SqlCommand sqlCommand = connection.CreateCommand();
+                sqlCommand.CommandText = sql;
+                sqlCommand.Parameters.AddWithValue("P1", person.Id);
+                sqlCommand.Parameters.AddWithValue("P2", person.FirstName);
+                sqlCommand.Parameters.AddWithValue("P3", person.Surname);
+                sqlCommand.Parameters.AddWithValue("P4", person.Birthday);
+
+                sqlCommand.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        public void Update(Person person)
+        {
+            using (var connection = new SqlConnection(this.ConnectionString))
+            {
+                var sql = @"UPDATE PEOPLE SET FirstName = @P1, Surname = @P2, Birthday = @P3 WHERE Id = @P4";
+
+                if (connection.State != ConnectionState.Open) { connection.Open(); }
+
+                SqlCommand sqlCommand = connection.CreateCommand();
+                sqlCommand.CommandText = sql;
+                sqlCommand.Parameters.AddWithValue("P1", person.FirstName);
+                sqlCommand.Parameters.AddWithValue("P2", person.Surname);
+                sqlCommand.Parameters.AddWithValue("P3", person.Birthday);
+                sqlCommand.Parameters.AddWithValue("P4", person.Id);
+                sqlCommand.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            using (var connection = new SqlConnection(this.ConnectionString))
+            {
+                var sql = @"DELETE FROM PEOPLE WHERE Id = @P1";
+
+                if (connection.State != ConnectionState.Open) { connection.Open(); };
+
+                SqlCommand sqlCommand = connection.CreateCommand();
+                sqlCommand.CommandText = sql;
+                sqlCommand.Parameters.AddWithValue("P1", id);
+                sqlCommand.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+        public List<Person> GetAll()
+        {
+            List<Person> result = new List<Person>();
+            using (var connection = new SqlConnection(this.ConnectionString))
+            {
+                var sql = @"SELECT ID, FirstName, Surname, Birthday FROM PEOPLE";
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                SqlCommand sqlCommand = connection.CreateCommand();
+                sqlCommand.CommandText = sql;
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Person person = new Person()
+                    {
+                        Id = reader.GetGuid("Id"),
+                        FirstName = reader.GetString("FirstName"),
+                        Surname = reader.GetString("Surname"),
+                        Birthday = reader.GetDateTime("Birthday")
+                    };
+                    result.Add(person);
+                }
+                connection.Close();
+                return result;
+            }
+        }
+
+        public Person GetById(Guid id)
+        {
+            List<Person> result = new List<Person>();
+            using (var connection = new SqlConnection(this.ConnectionString))
+            {
+                var sql = @"SELECT Id, FirstName, Surname, Birthday FROM PEOPLE WHERE Id = @P1";
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                SqlCommand sqlCommand = connection.CreateCommand();
+                sqlCommand.CommandText = sql;
+                sqlCommand.Parameters.AddWithValue("P1", id);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Person person = new Person()
+                    {
+                        Id = reader.GetGuid("Id"),
+                        FirstName = reader.GetString("FirstName"),
+                        Surname = reader.GetString("Surname"),
+                        Birthday = reader.GetDateTime("Birthday")
+                    };
+                    result.Add(person);
+                }
+                connection.Close();
+            }
+            return result.FirstOrDefault();
+        }
+
+        public List<Person> SearchPeople(string firstName, string surname) 
+        {
+            List<Person> result = new List<Person>();
+            using (var connection = new SqlConnection(this.ConnectionString))
+            {
+                var sql = $"SELECT * FROM People WHERE FirstName LIKE '%@P1%' OR Surname LIKE '%@P2%'";
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                SqlCommand sqlCommand = connection.CreateCommand();
+                sqlCommand.CommandText = sql;
+                sqlCommand.Parameters.AddWithValue("P1", firstName);
+                sqlCommand.Parameters.AddWithValue("P2", surname);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Person person = new Person()
+                    {
+                        Id = reader.GetGuid("Id"),
+                        FirstName = reader.GetString("FirstName"),
+                        Surname = reader.GetString("Surname"),
+                        Birthday = reader.GetDateTime("Birthday")
+                    };
+                    result.Add(person);
+                }
+                connection.Close();
+            }
+            return result;
+        }
+    }
+}
